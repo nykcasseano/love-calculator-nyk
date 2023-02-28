5fbe7c1f898548eb1a41c8d1b68234a698dc0173
nyk <nykcasseano13@gmail.com>
2023-02-28 20:31:37 -0300

    Versão inicial do código que corresponde ao exercício

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Seja bem vindo a Calculadora do Amor")
+name1 = input("Qual é o seu nome\n ")
+name2 = input("Qual o nomde dele/dela\n ")
+
+combined_string =name1 + name2
+lower_case_string = combined_string.lower()
