b25109c49488b5a77ca8a2019516962c134f176c
nyk <nykcasseano13@gmail.com>
2023-02-28 21:23:37 -0300

    Versão inicial do código que corresponde ao exercício

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ name2 = input("Qual o nomde dele/dela\n ")
 
 combined_string =name1 + name2
 lower_case_string = combined_string.lower()
+
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+love_score = str(true) + str(love)
+
+print(love_score)
