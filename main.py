1c82d6d2d75ca9a3ebb9bdd3c6d41ca3207a5204
nyk <nykcasseano13@gmail.com>
2023-02-28 23:04:35 -0300

    Versão final do código que corresponde ao exercício

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ e = lower_case_string.count("e")
 
 love = l + o + v + e
 
-love_score = str(true) + str(love)
+love_score = int(str(true) + str(love))
 
-print(love_score)
+if (love_score <10) or (love_score > 90):
+    print(f"Your love score is {love_score}, you go to together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your love score is {love_score}, you are alright together.")
+else: 
+    print(f"Your score is {love_score}")
